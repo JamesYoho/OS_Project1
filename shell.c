f89d6b3490a094c6b1e904d08ad7120093b69a6e
James Yoho <jamesyoho13@gmail.com>
2026-02-26 08:15:14 -0500

    Update from shell

--- shell.c
+++ shell.c
@@ -37,8 +37,6 @@ int shell_file_exists(char *file_path) {
   // use the stat() system call to check if a file exists
 }
 
-
-
 // This file takes the filename, filepath, and the size of the filepath
 // It checks if the file exists. If it does it sets the filepath to the path of the file and returns 0, otherwise it returns -1
 int shell_find_file(char *file_name, char *file_path, char file_path_size) {
@@ -88,79 +86,70 @@ int main (int argc, char *argv[]) {
    //run the shell
    bool exit = false;
    
-   while (!exit){
-	//1. display prompt and wait for user input
-	// generate some prompt 
-	// e.g. username@hostname:~/david$ ping 
-	char command[127];
-	char cwd[127];
-	char file_path[127];
-		
-	printf("username@hostname:%s$ ", getcwd(cwd, sizeof(cwd)));
-	fgets(command, sizeof(command), stdin);
-	fflush(stdout);
-	//2. filter out whitespace command 
-	command[strcspn(command, "\n")] = 0;
+	while (!exit){
+		// display prompt and wait for user input
+		// generate some prompt 
+		// e.g. username@hostname:~/david$ ping 
+		char command[127];
+		char cwd[127];
+		char file_path[127];
 	
+		printf("username@hostname:%s$ ", getcwd(cwd, sizeof(cwd)));
+		fgets(command, sizeof(command), stdin);
+		fflush(stdout);
 
-	//Take the command and split it into an array of strings
-	char* words[256];
-	int numWords = 0;
-	char* word = strtok(command, " ");
-	while(word != NULL){
-		words[numWords] = word;
-		numWords++;
-		word = strtok(NULL, " ");
+
+		// filter out whitespace command 
+		command[strcspn(command, "\n")] = 0;
+		
+		// Take the command and split it into an array of strings
+		char* words[256];
+		int numWords = 0;
+		char* word = strtok(command, " ");
+		while(word != NULL){
+			words[numWords] = word;
+			numWords++;
+			word = strtok(NULL, " ");
+		}
+
+		words[numWords] = NULL;
+
+		// if command line contains non-whitespace characters
+		if(numWords > 0){
+			//Checks for exit
+			if(strcmp(words[0], "exit") == 0){
+				exit = true;
+			} 
+
+			// Runs our cd command if cd was passed. 
+			else if (strcmp(words[0], "cd") == 0){
+				if (numWords > 1){
+					shell_change_dir(words[1]);
+				}
+				else{
+					shell_change_dir(getenv("HOME"));
+				}
+			}
+
+			// Check if command is a direct path and run the file or command
+			else if (strchr(words[0], '/')) {
+				if (shell_file_exists(words[0]) == 0){
+					shell_execute(words[0], words);
+				}
+				else{
+					printf("Command not found\n");
+				}
+			}
+
+			// Search for the command in the directories listed in the PATH environment variable
+			else if (shell_find_file(words[0], file_path, (char)sizeof(file_path)) == 0) {
+				// command found in PATH
+				shell_execute(file_path, words);
+			}
+
+			else {
+				printf("Invalid Command\n");
+			}
+		}
 	}
-
-	words[numWords] = NULL;
-
-	//3. if command line contains non-whitespace characters
-	if(numWords > 0){
-		//Checks for exit
-		if(strcmp(words[0], "exit") == 0){
-			exit = true;
-		} 
-
-		// Runs our cd command if cd was passed. 
-		else if (strcmp(words[0], "cd") == 0){
-			if (numWords > 1){
-				shell_change_dir(words[1]);
-			}
-			else{
-				shell_change_dir(getenv("HOME"));
-			}
-		}
-		// Check if the command is a direct path (contains a '/') and run the file/command if possible
-		else if (strchr(words[0], '/')) {
-			// absolute or relative path
-			if (shell_file_exists(words[0]) == 0){
-				shell_execute(words[0], words);
-			}
-			else{
-				printf("Command not found\n");
-			}
-		}
-
-		// Search for the command in the directories listed in the PATH environment variable
-		else if (shell_find_file(words[0], file_path, (char)sizeof(file_path)) == 0) {
-			// command found in PATH
-			shell_execute(file_path, words);
-		}
-
-		else {
-			printf("Invalid Command\n");
-		}
-
-	}
-	// //4. parse command line
-	// 	if the specified command is “exit”, terminate the program taking care to release any allocated resources.
-	// 	if the specified command is “cd”
-	// 		change the current working directory to the specified directory path using shell_change_dir()
-	// 	if the command is specified using an absolute path (e.g. /usr/bin/ls), exists in the user’s PATH (e.g. ls) or exists in the current folder (e.g. ./hello_world)
-	// 		execute the command using shell_execute()
-	// 	else
-	// 		report an error message
-    }
-   
 }
